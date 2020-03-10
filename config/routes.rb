62cefb0cab49c74b490d8ebb795658fe2e113dd8
Igor Cruz <igor_cruz_avelino@hotmail.com>
2020-03-10 16:31:33 -0300

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#homepage"
 
+  resources :article
+
   controller :pages do
     get 'hoempage', action: 'homepage', as: 'user_home'
   end
